f993ef44674335950223022eac93f74f0890e8a4
Elusive James <86646115+ElusiveJames@users.noreply.github.com>
2023-12-15 09:35:51 +0000

    hello print f

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+/**
+  * main - Using printf to dispaly a string
+  * Return: Always 0
+ */
+#include <stdio.h>
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
