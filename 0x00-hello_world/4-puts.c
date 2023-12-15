0087fe6a3c3bfe39b29d30aa0e91c9e333c9ae47
Elusive James <86646115+ElusiveJames@users.noreply.github.com>
2023-12-15 09:17:37 +0000

    hello puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+/**
+  * main - Function to print a string
+  * Return: Always 0i
+ */
+#include <stdio.h>
+int  main(void)
+{
+	 puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
