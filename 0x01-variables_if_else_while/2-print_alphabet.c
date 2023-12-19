1d7e75844b38a688dc4f23cf25b185b1dbe94782
Elusive James <86646115+ElusiveJames@users.noreply.github.com>
2023-12-19 09:12:50 +0000

    display aphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+  * main - display alphabets
+  * Return: 0 if success
+  */
+
+int  main(void)
+{
+char ch = 97;
+
+while (ch < 123)
+{
+	putchar(ch);
+	++ch;
+}
+putchar('\n');
+return (0);
+}
