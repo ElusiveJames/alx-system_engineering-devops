c418dc0752f7ea3e6d2a5563d4ae2b4c5f4e4578
Elusive James <86646115+ElusiveJames@users.noreply.github.com>
2023-12-19 11:49:41 +0000

    persistence and patience is success

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+  * main - print single digit number combination
+  * Return: 0 success
+  */
+
+int main(void)
+{
+	int n = 48;
+
+	while (n <= 57)
+	{
+		putchar(n);
+		putchar(44);
+		putchar(32);
+		++n;
+	}
+	putchar('\n');
+	return (0);
+}
