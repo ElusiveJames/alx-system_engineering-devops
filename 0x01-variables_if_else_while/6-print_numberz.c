64312f6fecc31e3a25462664068d8e8dab351b76
Elusive James <86646115+ElusiveJames@users.noreply.github.com>
2023-12-19 10:22:05 +0000

    numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+  * main - print single digit number base 10
+  * Return: 0 success
+  */
+
+int main(void)
+{
+	int n = 48;
+
+	while (n <= 56)
+	{
+		putchar(n);
+		++n;
+	}
+	putchar(n);
+	return (0);
+}
